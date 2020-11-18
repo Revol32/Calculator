89c0bdf61603b14fa3423768abf63378c7a9e657
Revol32 <72274775+Revol32@users.noreply.github.com>
2020-11-18 18:54:11 +0400

    done

--- src/main/java/service/Calculate.java
+++ src/main/java/service/Calculate.java
@@ -1,5 +1,26 @@
 package service;
 
-public class Calculate {
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
+public class Calculate {
+    public String calculate(ArrayList<String> expressionList) {
+        double result = 0;
+        for (int i = 0; expressionList.size() > i; i++) {
+            if (expressionList.get(i).matches("[" + Operators.getOperatorsToStringWishOutBrackets() + "]")) {
+                System.out.println(expressionList.toString());
+                switch (expressionList.get(i)) {
+                    case "+" -> result = Double.parseDouble(expressionList.get(i - 2)) + Double.parseDouble(expressionList.get(i - 1));
+                    case "-" -> result = Double.parseDouble(expressionList.get(i - 2)) - Double.parseDouble(expressionList.get(i - 1));
+                    case "*" -> result = Double.parseDouble(expressionList.get(i - 2)) * Double.parseDouble(expressionList.get(i - 1));
+                    case "/" -> result = Double.parseDouble(expressionList.get(i - 2))/ Double.parseDouble(expressionList.get(i - 1));
+                }
+                expressionList.set(i - 2, String.valueOf(result));
+                expressionList.remove(i);
+                expressionList.remove(i - 1);
+                i = 0;
+            }
+        }
+        return new DecimalFormat("#.#######################").format(result);
+    }
 }
--- src/main/java/service/Operators.java
+++ src/main/java/service/Operators.java
@@ -1,4 +1,49 @@
 package service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Operators {
+    static final Map<String, Integer> operators;
+
+    static {
+        operators = new HashMap<>();
+        operators.put("(", 0);
+        operators.put(")", 0);
+        operators.put("+", 1);
+        operators.put("-", 1);
+        operators.put("*", 2);
+        operators.put("/", 2);
+    }
+
+    public static String getOperatorsToStringWishOutBrackets() {
+        StringBuilder operatorsString = new StringBuilder();
+        for (String operator : operators.keySet()) {
+            if (!operator.equals("(") && !operator.equals(")")) {
+                operatorsString.append("\\").append(operator);
+            }
+        }
+        return operatorsString.toString();
+    }
+
+    public static String getOperatorsToString() {
+        StringBuilder operatorsString = new StringBuilder();
+        for (String operator : operators.keySet()) {
+            operatorsString.append("\\").append(operator);
+        }
+        return operatorsString.toString();
+    }
+
+    public static String getOperatorsHighPriorityToString() {
+        StringBuilder operatorsString = new StringBuilder();
+        for (String operator : operators.keySet()) {
+            if (operators.get(operator) >= 2)
+                operatorsString.append("\\").append(operator);
+        }
+        return operatorsString.toString();
+    }
+
+    public static int getOperatorPriority(String operatorFind) {
+        return operators.get(operatorFind);
+    }
 }
